eaa150e4f8cebf55391052474a381b805689d261
emarskiy <oldfattroll@gmail.com>
2022-08-08 23:11:19 +0300

    функция makeFriends, выделение ячеек по диагонали, функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  let cells = table.querySelectorAll('td');
+
+  cells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.dataset.available === 'true') {
+      row.classList.add('available');
+    } else if (cell.dataset.available === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.hidden = true;
+    }
+  });
+
+  cells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.textContent < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  });
+
+  cells.forEach(cell => {
+    let row = cell.closest('tr');
+
+    if (cell.textContent === "m") {
+      row.classList.add("male");
+    } else {
+      row.classList.add("female");
+    }
+  });
 }
